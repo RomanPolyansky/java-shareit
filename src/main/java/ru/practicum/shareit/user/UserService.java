543761c6d578c13acb5888d76f18ddc3a5704eea
Roman Polyansky <86264905+RomanPolyansky@users.noreply.github.com>
2022-12-28 20:18:25 +0300

    successfully forgot to work in branch

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.user;
-
-import ru.practicum.shareit.abstracts.GeneralService;
-import ru.practicum.shareit.user.model.User;
-
-
-interface UserService extends GeneralService<User> {
-}
--- src/main/java/ru/practicum/shareit/user/UserStorage.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.user;
-
-import ru.practicum.shareit.abstracts.GeneralStorage;
-import ru.practicum.shareit.user.model.User;
-
-public interface UserStorage extends GeneralStorage<User> {
-}
