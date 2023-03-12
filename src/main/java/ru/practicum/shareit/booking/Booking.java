cb16d70f76ee4b196aa3f589d5026bc9290c5388
Roman Polyansky <roman_polyan@mail.ru>
2023-03-12 23:36:00 +0300

    +Booking JPA TODO other JPA

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,43 @@
 package ru.practicum.shareit.booking;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.Instant;
+
 /**
  * TODO Sprint add-bookings.
  */
+
+@Entity
+@Table(name = "bookings")
+@Data
+@Builder
+@AllArgsConstructor
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "start_date")
+    private Instant startDate;
+
+    @Column(name = "end_date")
+    private Instant endDate;
+
+    @Column(name = "booker_id")
+    private long bookerId;
+
+    @Column(name = "item_id")
+    private long itemId;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Booking() {
+    }
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -3,16 +3,28 @@ package ru.practicum.shareit.user.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.*;
 
 /**
  * TODO Sprint add-controllers.
  */
+
+@Entity
+@Table(name = "users")
 @Data
 @Builder
 @AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email", nullable = false)
     private String email;
 
     public User() {
