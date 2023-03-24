0023e8eaec15f059cd438ab67c929aabad389f98
Roman Polyansky <roman_polyan@mail.ru>
2023-03-24 17:30:25 +0300

    Coming on to Review #2

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-import ru.practicum.shareit.user.UserRepositoryCustom;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
-    Optional<User> findByEmail(@Param("email") String emailSearch);
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
